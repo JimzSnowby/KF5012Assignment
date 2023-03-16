4355f092effafcfe5acac4f04af169fdcf2a77ce
JimzSnowby <117113270+JimzSnowby@users.noreply.github.com>
2023-03-16 13:31:56 +0000

    Changed task terminology to Chore

---
+++ src/main/java/com/mycompany/kf5012assessment/ChoreList.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.kf5012assessment;
+
+import java.util.*;
+/**
+ *
+ * @author w21023500
+ */
+public class ChoreList {
+    private List<Chore> choreList = new ArrayList();
+
+    public List<Chore> getChoreList() {
+        return choreList;
+    }
+
+    public void setChoreList(List<Chore> choreList) {
+        this.choreList = choreList;
+    }
+    
+}
