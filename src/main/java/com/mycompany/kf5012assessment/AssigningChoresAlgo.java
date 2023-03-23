3465258250fa766db2acde3d236d5c63d2a77f56
JimzSnowby <117113270+JimzSnowby@users.noreply.github.com>
2023-03-23 14:47:39 +0000

    shit

--- src/main/java/com/mycompany/kf5012assessment/AssigningChoresAlgo.java
+++ src/main/java/com/mycompany/kf5012assessment/AssigningChoresAlgo.java
@@ -4,6 +4,11 @@
  */
 package com.mycompany.kf5012assessment;
 
+import java.sql.SQLException;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author w21023500
@@ -11,10 +16,27 @@ package com.mycompany.kf5012assessment;
 // For Task 7 Group work
 public class AssigningChoresAlgo {
 
-    ChoresDatabase db = new ChoresDatabase();
+    AssignedChoresList user1 = new AssignedChoresList();
+    AssignedChoresList user2 = new AssignedChoresList();
+    
+
     
     public static void main(String[] args) {
-        
+        /*ChoresDatabase db = new ChoresDatabase();
+        try {
+            db.selectChores();
+        } catch (SQLException ex) {
+            Logger.getLogger(AssigningChoresAlgo.class.getName()).log(Level.SEVERE, null, ex);
+        }*/
+        NormaliseTotalTime();
     }
-    
+   
+    public void NormaliseTotalTime() {
+        ArrayList<Chore> total = new ArrayList();
+        user1.getAssignedChoresList();
+        for(int i = 0; i < user1.getAssignedChoresList().size(); i++){
+            System.out.println("Loop" + i);
+        }
+
+    }    
 }
