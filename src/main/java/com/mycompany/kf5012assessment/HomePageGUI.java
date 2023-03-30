3e9634631cae57395d616935d6855aadedc673e4
Yoyo <124893353+yoyosiu0519@users.noreply.github.com>
2023-03-30 15:56:31 +0100

    Change the Home page GUI name
    nothing that important

--- src/main/java/com/mycompany/kf5012assessment/HomePageGUI.java
+++ src/main/java/com/mycompany/kf5012assessment/HomePageGUI.java
@@ -8,12 +8,12 @@ package com.mycompany.kf5012assessment;
  *
  * @author w21023500
  */
-public class StartScreenGUI extends javax.swing.JFrame {
+public class HomePageGUI extends javax.swing.JFrame {
 
     /**
      * Creates new form StartScreenGUI
      */
-    public StartScreenGUI() {
+    public HomePageGUI() {
         initComponents();
     }
 
@@ -100,20 +100,21 @@ public class StartScreenGUI extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(StartScreenGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HomePageGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(StartScreenGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HomePageGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(StartScreenGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HomePageGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(StartScreenGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(HomePageGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new StartScreenGUI().setVisible(true);
+                new HomePageGUI().setVisible(true);
             }
         });
     }
