45c62c67ba6d5309bd14f4ff3a75c42766905547
Maja <maja.bosy@northumbria.ac.uk>
2023-04-09 17:51:38 +0100

    update

--- src/main/java/com/mycompany/kf5012assessment/ChoresDatabase.java
+++ src/main/java/com/mycompany/kf5012assessment/ChoresDatabase.java
@@ -27,6 +27,8 @@ public class ChoresDatabase {
      *
      * Functions below
      *
+     * 
+     * 
      */
     //Select all chores
     public ArrayList<Chore> selectChores() throws SQLException {
@@ -54,6 +56,8 @@ public class ChoresDatabase {
         }
         return chores;
     }
+    
+  
 
     //Select weekly chores
     public ArrayList<Chore> selectChoresFrequencyWeekly() throws SQLException {
